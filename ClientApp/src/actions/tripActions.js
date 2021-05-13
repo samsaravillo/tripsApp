271ae2957422f562b26446fbdb642ac93af9e967
samiwokx <samsaravillo@gmail.com>
2021-05-14 01:10:52 +0800

    update tripActions

---
+++ ClientApp/src/actions/tripActions.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const GET_ALL_TRIPS_REQUEST = 'GET_ALL_TRIPS_REQUEST';
+export const GET_ALL_TRIPS_SUCCESS = 'GET_ALL_TRIPS_SUCCESS';
+export const GET_ALL_TRIPS_ERROR = 'GET_ALL_TRIPS_ERROR';
